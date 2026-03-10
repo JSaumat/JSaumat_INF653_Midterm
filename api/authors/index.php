054510f68d7a54780e8cc8a753b94daa24e0af8f
JSaumat <j_saumat@fhsu.mail.edu>
2026-03-09 19:18:06 -0700

    Add CORS headers for automated API testing

--- api/authors/index.php
+++ api/authors/index.php
@@ -16,7 +16,15 @@ GET    - Retrieve all authors or a specific author by ID
 POST   - Create a new author
 */
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+  http_response_code(200);
+  exit;
+}
 
 require_once "../../config/Database.php";
 require_once "../../models/Author.php";
--- api/quotes/index.php
+++ api/quotes/index.php
@@ -19,7 +19,15 @@ PUT    - Update an existing quote
 DELETE - Remove a quote from the database
 */
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+  http_response_code(200);
+  exit;
+}
 
 require_once "../../config/Database.php";
 require_once "../../models/Quote.php";
